6f3433b9eafcdb7e0487390c9b50400d282b2ee6
Nicholas <spammiehere@gmail.com>
2022-01-12 00:08:35 -0500

    Add initial library styling/functionality

---
+++ library/script.js
@@ -0,0 +1,84 @@
+const statusReadPara = document.querySelectorAll('.status-read')
+const statusNotReadPara = document.querySelectorAll('.status-not-read')
+const addBookButton = document.querySelector('button')
+const bookContainer = document.querySelector('.container')
+
+function Book(title, author, pages, rating, readStatus){
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.rating = rating;
+  this.readStatus = readStatus;
+
+  this.info = function() {
+    return `This is ${title}, a book by ${author}. It is ${pages} pages long and it ${readStatus}. It is rated ${rating}/10.`;
+  }
+}
+
+let myLibrary = [];
+
+const theHobbit = new Book("The Hobbit", "J.R.R Tolkien", 350, 8, true)
+const ballaBook = new Book("The balla", "J.R.R Ballin", 360, 3, false)
+
+function addBookToLibrary(){
+  //Takes user input and creates a new Book entry
+  console.log("Yes")
+
+}
+
+function updateBookList(){
+  //Function will take the array of books and add a book/remove a book when it is added/removed from the array
+}
+
+function addBookDiv(Book){
+
+  const newBookDiv = document.createElement('div')
+  const newBookTitle = document.createElement('h2')
+  const newBookAuthor = document.createElement('p')
+  const newBookPages = document.createElement('p')
+  const newBookRating = document.createElement('p')
+  const newBookStatus = document.createElement('p')
+
+  newBookDiv.append(newBookTitle, newBookAuthor, newBookPages, newBookRating, newBookStatus)
+
+  newBookTitle.textContent = Book.title;
+  newBookAuthor.textContent = `by ${Book.author}`
+  newBookPages.textContent = `${Book.pages} Pages`
+  newBookRating.textContent = `Rating: ${Book.rating}/10`
+
+  newBookDiv.classList.add("book")
+  newBookAuthor.classList.add("author")
+  newBookPages.classList.add("pages")
+  newBookRating.classList.add("rating")
+  
+  if(Book.readStatus){
+    newBookStatus.textContent = "Read"
+    newBookStatus.classList.remove("status-not-read")
+    newBookStatus.classList.add("status-read")
+
+  } else {
+    newBookStatus.textContent = "Not Read"
+    newBookStatus.classList.remove("status-read")
+    newBookStatus.classList.add("status-not-read")
+  }
+
+
+  bookContainer.prepend(newBookDiv)
+
+}
+
+
+addBookButton.addEventListener('click', addBookToLibrary)
+
+/* PsuedoCode: 
+
+1. User clicks "Add Book"
+2. Form pops up for user input
+3. User enters information/status
+4. User clicks "Add"
+5. Book is added to the DOM
+6. User clicks "Click to read"
+6. User clicks "Delete book"
+7. Book is removed from list 
+
+*/
