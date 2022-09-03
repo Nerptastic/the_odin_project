ff3a13b05296aed6d1cfc72d0a89b2cf38c96863
Nicholas <spammiehere@gmail.com>
2022-09-02 23:45:12 -0400

    Remove old files

--- restaurant-page/old/src/content.js
+++
@@ -1,4 +0,0 @@
-export default function content(sectionID) {
-    document.getElementById('section').innerHTML = document.getElementById(sectionID).innerHTML;
-  return sectionID;
-}
--- restaurant-page/old/src/footer.js
+++
@@ -1,7 +0,0 @@
-export default function footer() {
-  const footer = document.createElement('footer');
-  footer.setAttribute('id', 'footer');
-  footer.textContent = "Copyright © Tyco Goon Scrap Restaurant";
-  console.log("Footer Loaded")
-  return footer;
-}
